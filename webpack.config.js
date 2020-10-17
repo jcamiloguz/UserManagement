bb878eebc0acc75b1d73186eee9b57d10d4aa31e
Juan Camilo Guzman <jcamiloguz1@gmail.com>
2020-10-17 11:01:48 -0500

    Setup: finish webpack setup

---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const config = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+		filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        use: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: [
+					{
+						loader:MiniCssExtractPlugin.loader,
+					},
+          'css-loader'
+        ]
+      }
+    ]
+  },
+  resolve: {
+    extensions: [
+      '.js',
+      '.jsx'
+    ]
+	},
+	devServer: {
+    historyApiFallback: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+      filename: 'index.html'
+		}),
+		new MiniCssExtractPlugin(
+			{filename:'assets/[name].css'}
+		)
+  ]
+};
+
+module.exports = config;
