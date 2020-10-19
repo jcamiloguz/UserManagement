45e6456c0f9a2842865d09c77c0f4c66cb938eda
Juan Camilo Guzman <jcamiloguz1@gmail.com>
2020-10-19 05:52:32 -0500

    edit form: reacting

---
+++ src/api.js
@@ -0,0 +1,70 @@
+export const createUser = async (user) => {
+  const active = user.active === 'yes' ? true : false
+  const postBody = {
+    firstName: user.firstName,
+    lastName: user.lastName,
+    userName: user.userName,
+    email: user.email,
+    active: active,
+  }
+  console.log(JSON.stringify(postBody))
+  const response = await fetch(
+    `https://jcamiloguz-userapi.herokuapp.com/users`,
+    {
+      method: 'POST',
+      mode: 'cors',
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+      body: JSON.stringify(postBody),
+    }
+  )
+	const data = await response.json()
+	console.log(data)
+  if (user.roleAdmin) {
+    const Adminresponse = await fetch(
+      `https://jcamiloguz-userapi.herokuapp.com/user_roles/${data.id}/1`,
+      {
+				method: 'POST',
+        mode: 'cors',
+        headers: {
+					'Content-type': 'application/json; charset=UTF-8',
+        },
+      }
+			)
+		}
+		if (user.roleMember) {
+			const Memberresponse = await fetch(
+				`https://jcamiloguz-userapi.herokuapp.com/user_roles/${data.id}/11`,
+				{
+					method: 'POST',
+					mode: 'cors',
+					headers: {
+						'Content-type': 'application/json; charset=UTF-8',
+					},
+				}
+				)
+			}
+			if (user.roleContr) {
+				const Contrresponse = await fetch(
+					`https://jcamiloguz-userapi.herokuapp.com/user_roles/${data.id}/21`,
+      {
+        method: 'POST',
+        mode: 'cors',
+        headers: {
+          'Content-type': 'application/json; charset=UTF-8',
+        },
+      }
+    )
+  }
+}
+export const deleteUser = (data	) => {
+	fetch(`https://jcamiloguz-userapi.herokuapp.com/users/${data.idUser}`, {
+		method: 'DELETE',
+		headers: {
+			'Content-type': 'application/json; charset=UTF-8',
+		},
+	})
+
+	
+}
--- src/components/UserForm/index.js
+++ src/components/UserForm/index.js
@@ -1,129 +1,147 @@
-import React,{useEffect, useState} from 'react'
-import {Form, Input} from './styles'
-import {Button} from '../Button'
+import React, { useEffect, useState } from 'react'
+import { Form, Input } from './styles'
+import { Button } from '../Button'
 import { useHistory } from 'react-router'
+import { createUser, deleteUser } from '../../api'
 
+export const UserForm = ({ data={}, type = 'new' }) => {
+  const [state, setState] = useState({
+    error: null,
+    user: {
+      firstName: '',
+      lastName: '',
+      email: '',
+      userName: '',
+      roles: [],
+      active: data.active===1 ? 'yes': 'no',
+    },
+  })
+  const history = useHistory()
 
-export const UserForm =({data,type='new'})=>{
-	const [state, setState]=useState({error:null,user:{firstName:'',lastName:'',email:'',userName:'',roles:[],active:'no'}})
-	const history=useHistory()
+  useEffect(() => {
+		
+    if (type !== 'new') {
+      fetch(
+        `https://jcamiloguz-userapi.herokuapp.com/user_roles/user/${data.idUser}`
+      )
+        .then((res) => res.json())
+        .then((res) => {
+					console.log(res)
+        })
+    }
+  }, [])
+  const changeHandler = (e) => {
+    console.log(e.target.name + ' ' + e.target.value)
+    if (e.target.value === '') {
+      setState({
+        ...state,
+        error: `Fill ${e.target.name} input`,
+        user: { ...state.user, [e.target.name]: e.target.value },
+      })
+    } else {
+      setState({
+        ...state,
+        error: null,
+        user: { ...state.user, [e.target.name]: e.target.value },
+      })
+    }
+  }
 
-	useEffect(()=>{
-		if(type!=='new'){
-			fetch(`https://jcamiloguz-userapi.herokuapp.com/user_roles/user/${data.idUser}`)
-			.then((res)=>res.json())
-			.then(res=>{setState(...state,{user:{...data , roles:res}})})
-		}
-		},[])
-	const changeHandler=(e)=>{
-
-			if(e.target.value===''){
-				setState({...state,error:`Fill ${e.target.name} input`,user:{...state.user,
-					[e.target.name]:e.target.value}})
-			}else{
-				setState({...state,error:null,user:{...state.user,
-					[e.target.name]:e.target.value}})
-		}
-	}
-	
-	const deleteHandler=(e)=>{
+  const deleteHandler = async (e) => {
 		e.preventDefault()
-		fetch(`https://jcamiloguz-userapi.herokuapp.com/users/${data.idUser}`,{
-			method: 'DELETE', 
-			headers: {
-			 'Content-type': 'application/json; charset=UTF-8' 
-			},
-		 })
-		 .then(res=>res.json)
-		 .catch(err=>console.log(err))
-		 .finally(()=>history.go(0))
-
-	}
-	const addHandler=(e)=>{
-		e.preventDefault()
-		const active = state.user.active==='yes'?true:false
-		const postBody ={
-			firstName:state.user.firstName,
-			lastName:state.user.lastName,
-			userName:state.user.userName,
-			email:state.user.email,
-			active:active,
-		}
-		console.log(JSON.stringify(postBody))
-		fetch(`https://jcamiloguz-userapi.herokuapp.com/users`,{
-			method: 'POST', 
-			mode: 'cors', 
-			headers: {
-				'Content-type': 'application/json; charset=UTF-8' 
-			},
-			body: JSON.stringify(postBody) 
-		})
-		.then(res=>console.log(res))
-	}
-	return(
-		<Form>
-			<div >
-				<h2>First Name:</h2>
-				<Input name='firstName'onChange={changeHandler}value={state.user.firstName || ''}/>
-			</div>
-			<div >
-				<h2>Last Name:</h2>
-				<Input name='lastName'onChange={changeHandler} value={state.user.lastName || ''}/>
-			</div>
-			<div >
-				<h2>Email:</h2>
-				<Input name='email' onChange={changeHandler}type='email'value={state.user.email || ''}/>
-			</div>
-			<div >
-				<h2>Username:</h2>
-				<Input name='userName'onChange={changeHandler}value={state.user.userName || ''}/>
-			</div>
-			<div className="checkboxes">
-				<h2>User role(s):</h2>
-				<label>
-  				<input type="checkbox" name='roleAdmin' onChange={changeHandler}  />
-					<span className="checkmark">
-  				</span>
-					Administrator
-				</label>
-				<label>
-  				<input type="checkbox" name='roleMember' onChange={changeHandler}  />
-  				<span className="checkmark"></span>
-					Member
-				</label>
-				<label>
-  				<input type="checkbox" name='roleContr' onChange={changeHandler}  />
-  				<span className="checkmark"></span>
-					Contributor
-				</label>
-			</div>
-			<div>
-				<h2>User active:</h2>
-				  <input type="radio" id='yes' name="active" value='yes'onChange={changeHandler} checked={state.user.active==='yes'} />
-				<label>
-					Yes
-				</label>
-				  <input type="radio" id='no' name="active" value='no'onChange={changeHandler} checked={state.user.active==='no'}/>
-				<label>
-					No
-				</label>
-			</div>
-			{
-			state.error!==null
-			&& <h4>Error: {state.error}</h4>
-			}
-			<div className="buttons">
-				{
-					type==='new'
-					?
-					<Button  clickHandler={addHandler} type="add"/>
-					:<> 
-					<Button clickHandler={deleteHandler}type="delete"/>
-					<Button type="save"/></>
-				}
-				
-			</div>
-
-		</Form>
-	)
+		await deleteUser(data)
+    history.go(0)
+  }
+  const addHandler = async (e) => {
+    e.preventDefault()
+    await createUser(state.user)
+    history.push('/users')
+  }
+  return (
+    <Form>
+      <div>
+        <h2>First Name:</h2>
+        <Input
+          name="firstName"
+          onChange={changeHandler}
+          placeholder={data.firstName || ''}
+        />
+      </div>
+      <div>
+        <h2>Last Name:</h2>
+        <Input
+          name="lastName"
+          onChange={changeHandler}
+          placeholder={data.lastName || ''}
+        />
+      </div>
+      <div>
+        <h2>Email:</h2>
+        <Input
+          name="email"
+          onChange={changeHandler}
+          type="email"
+          placeholder={data.email || ''}
+        />
+      </div>
+      <div>
+        <h2>Username:</h2>
+        <Input
+          name="userName"
+          onChange={changeHandler}
+          placeholder={data.userName || ''}
+        />
+      </div>
+      <div className="checkboxes">
+        <h2>User role(s):</h2>
+        <label>
+          <input type="checkbox" name="roleAdmin" onChange={changeHandler} />
+          <span className="checkmark"></span>
+          Administrator
+        </label>
+        <label>
+          <input type="checkbox" name="roleMember" onChange={changeHandler} />
+          <span className="checkmark"></span>
+          Member
+        </label>
+        <label>
+          <input type="checkbox" name="roleContr" onChange={changeHandler} />
+          <span className="checkmark"></span>
+          Contributor
+        </label>
+      </div>
+      <div>
+        <h2>User active:</h2>
+        <input
+          type="radio"
+          id="yes"
+          name="active"
+          value="yes"
+          onChange={changeHandler}
+          checked={state.user.active === 'yes'}
+        />
+        <label>Yes</label>
+        <input
+          type="radio"
+          id="no"
+          name="active"
+          value="no"
+          onChange={changeHandler}
+          checked={state.user.active === 'no'}
+        />
+        <label>No</label>
+      </div>
+      {state.error !== null && <h4>Error: {state.error}</h4>}
+      <div className="buttons">
+        {type === 'new' ? (
+          <Button clickHandler={addHandler} type="add" />
+        ) : (
+          <>
+            <Button clickHandler={deleteHandler} type="delete" />
+            <Button type="save" />
+          </>
+        )}
+      </div>
+    </Form>
+  )
 }
